9008a315e6c53dddeaa66d2b63032350f9340102
Daniel Kingsbury <dpk14@duke.edu>
2020-10-23 00:05:48 -0400

    fixed exception throw in LoginDBInit

--- src/main/java/Model/Data/API/Initialization/LoginDBInit.java
+++ src/main/java/Model/Data/API/Initialization/LoginDBInit.java
@@ -1,12 +1,13 @@
 package Model.Data.API.Initialization;
 
 import Model.Data.SQL.TableNames;
+import Model.ErrorHandling.Exceptions.ServerExceptions.LoadPropertiesException;
 import Model.ErrorHandling.Exceptions.ServerExceptions.TableExceptions.CreateTableException;
 import Model.ErrorHandling.Exceptions.ServerExceptions.TableExceptions.RemoveTableException;
 
 public class LoginDBInit extends InitDBAPI {
 
-    public LoginDBInit() {super();}
+    public LoginDBInit() throws LoadPropertiesException {super();}
 
     @Override
     public void createTablesIfNull() throws CreateTableException {
