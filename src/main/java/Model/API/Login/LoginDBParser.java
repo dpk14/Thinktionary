7226a7b1a0d27529de6c8440d2743bc72c3daba0
Daniel Kingsbury <dpk14@duke.edu>
2020-10-27 21:49:14 -0400

    removed unused method

--- src/main/java/Model/API/Login/LoginDBParser.java
+++ src/main/java/Model/API/Login/LoginDBParser.java
@@ -1,22 +1,11 @@
 package Model.API.Login;
 
 import Model.Data.SQL.ColumnInfo;
-import java.util.HashMap;
+
 import java.util.List;
 import java.util.Map;
 
 public class LoginDBParser {
-    public static Map<Integer, User> parseUserInfoMap(List<Map<String, Object>> table) throws ClassCastException, RuntimeException {
-        Map<Integer, User> ret = new HashMap();
-        for(Map<String, Object> row : table){
-            int id = Integer.parseInt((String) row.get(ColumnInfo.getUSERID()));
-            String userName = (String) row.get(ColumnInfo.getUSERNAME());
-            String password = (String) row.get(ColumnInfo.getPASSWORD());
-            User user = new User(id, userName, password);
-            ret.put(id, user);
-        }
-        return ret;
-    }
 
     public static int getUserID(List<Map<String, Object>> userInfo) throws ClassCastException{
         return (int) userInfo.get(0).get(ColumnInfo.getUSERID());
